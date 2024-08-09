db42ff62b30dd87b4b23f198f75de2823ed24143
Nengyi-Jonathan-Jiang <theNerdyMango@gmail.com>
2024-08-08 21:03:38 -0500

    refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <link rel="icon" type="image/x-icon" href="../_assets/favicon.ico"/>
+                <link rel="icon" type="image/x-icon" href="./favicon.ico"/>
             </head>
             <body>{children}</body>
         </html>
